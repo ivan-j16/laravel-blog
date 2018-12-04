cb9190cadf4d616528675a872a622c782595dc05
Jirov,Ivan I <i.jirov@student.fontys.nl>
2018-12-04 11:48:28 +0100

    Added seeding with factory

--- web3App/resources/views/posts/show.blade.php
+++ web3App/resources/views/posts/show.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
     <h1>{{$post->title}}</h1>
+    <img style="width: 100%" src = "/storage/cover_images/{{$post->cover_image}}">
+    <br>
+    <br>
     <div>
         {!! $post->body !!}
     </div>
